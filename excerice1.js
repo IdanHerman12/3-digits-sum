acb2e18ed9d9886189d59e714cde1557f4c0781b
IdanHerman12 <idanh156@gmail.com>
2021-08-29 16:05:32 +0300

    finsihed after a few tests

--- excerice1.js
+++ excerice1.js
@@ -1,9 +1,9 @@
 'use strict';
-const num = 123;
+const num = 897;
 let sum = 0;
 sum+=num%10;
 console.log(sum);
 sum+=Math.floor(num/10%10);
 console.log(sum);
-sum+=Math.floor(num%100);
+sum+=Math.floor(num/100);
 console.log(sum);
